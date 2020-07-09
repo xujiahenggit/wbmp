a2badc51f049c8cd285bc7a6c05646137ba0b037
丁彦文 <wx2020064@dev.bcs>
2020-07-09 15:54:08 +0800

    现场检查接口 请求字段备注描述优化

--- icop/src/main/java/com/bank/icop/dto/CheckPonitSaveDTO.java
+++ icop/src/main/java/com/bank/icop/dto/CheckPonitSaveDTO.java
@@ -2,6 +2,7 @@ package com.bank.icop.dto;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -26,19 +27,27 @@ public class CheckPonitSaveDTO implements Serializable {
     */
     private static final long serialVersionUID = -2608359063079418125L;
 
+    @ApiModelProperty(value = "检查任务主键PK")
     private String taskPk;
 
+    @ApiModelProperty(value = "检查任务项主键PK")
     private String ePk;
 
+    @ApiModelProperty(value = "检查笔数")
     private String checkNumber;
 
+    @ApiModelProperty(value = "检查开始时间")
     private String checkStartDate;
 
+    @ApiModelProperty(value = "检查结束时间")
     private String checkEndDate;
 
+    @ApiModelProperty(value = "检查手段")
     private String checkMethod;
 
+    @ApiModelProperty(value = "检查记录")
     private String checkDes;
 
+    @ApiModelProperty(value = "其他/回头看")
     private String checkOther;
 }
--- icop/src/main/java/com/bank/icop/dto/CheckTaskSaveDTO.java
+++ icop/src/main/java/com/bank/icop/dto/CheckTaskSaveDTO.java
@@ -3,6 +3,7 @@ package com.bank.icop.dto;
 import java.io.Serializable;
 import java.util.List;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -27,7 +28,9 @@ public class CheckTaskSaveDTO implements Serializable {
     */
     private static final long serialVersionUID = 5395738676165401215L;
 
+    @ApiModelProperty(value = "检查过程数据项")
     private CheckPonitSaveDTO checkpoints;
 
+    @ApiModelProperty(value = "检查问题数据项列表")
     private List<CheckProblemSaveDTO> list;
 }
