0f4655c17d1cdd2ddae6f775ee4797d9156f73a9
张富强 <wx2020065@dev.bcs>
2020-07-13 10:30:32 +0800

    1、工单流水表自动生成代码

---
+++ manage/src/main/java/com/bank/manage/service/WorkWaterService.java
@@ -0,0 +1,24 @@
+package com.bank.manage.service;
+
+
+import com.bank.core.entity.PageQueryModel;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.bank.manage.dos.WorkWaterDO;
+import com.bank.manage.vo.WorkWaterVO;
+import com.baomidou.mybatisplus.extension.service.IService;
+/**
+ * 工单流水表 服务类
+ *
+ * @author 代码自动生成
+ * @since 2020-07-13
+ */
+public interface WorkWaterService extends IService<WorkWaterDO> {
+
+    /**
+     * 自定义分页
+     * @param pageQueryModel
+     * @return
+     */
+    IPage<WorkWaterDO> listPage(PageQueryModel pageQueryModel);
+
+}
