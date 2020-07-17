f08b2fc7ed4cf879b99fd3477549ae2f7fd3c48e
zhaozhongyuan <zhaozhongyuan@163.com>
2020-07-17 17:34:16 +0800

    附件地址保存影响平台的url

--- esb/src/main/java/com/bank/esb/dto/TransferInformationDto.java
+++ esb/src/main/java/com/bank/esb/dto/TransferInformationDto.java
@@ -1,6 +1,7 @@
 package com.bank.esb.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class TransferInformationDto {
     @ApiModelProperty(value = "操作人ID")
     private String operatorUserId;
