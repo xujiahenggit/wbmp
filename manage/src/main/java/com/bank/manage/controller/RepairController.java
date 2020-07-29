0222693a5158cb8612b4326f4671e1eea69b6054
文德戈 <wx2020021@dev.bcs>
2020-07-29 09:52:27 +0800

    Merge branch 'wbmp-715' of D:\wbmp-server with conflicts.

--- manage/src/main/java/com/bank/manage/controller/RepairController.java
+++ manage/src/main/java/com/bank/manage/controller/RepairController.java
@@ -1,13 +1,9 @@
 package com.bank.manage.controller;
 
-import com.bank.auth.base.BaseController;
 import com.bank.core.entity.BizException;
-import com.bank.core.entity.ImagePlatformFile;
-import com.bank.core.entity.ImagePlatformResponse;
-import com.bank.core.entity.TokenUserInfo;
-import com.bank.core.utils.ImagePlatformUtils;
 import com.bank.manage.dao.InspectionEquipmentDto;
 import com.bank.manage.dao.LargerScreenDto;
+import com.bank.manage.dos.ManageWorkOrderAttachmentDO;
 import com.bank.manage.dos.ManageWorkOrderDO;
 import com.bank.manage.dos.WorkWaterDO;
 import com.bank.manage.dto.*;
@@ -16,20 +12,19 @@ import com.bank.manage.service.ManageWorkWaterService;
 import com.bank.manage.service.RepairService;
 import com.bank.manage.util.Tools;
 import com.bank.manage.vo.*;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 /***
@@ -39,7 +34,7 @@ import java.util.*;
 @Api(tags = "监控平台-工单信息接口")
 @RestController
 @RequestMapping("/repair")
-public class RepairController extends BaseController {
+public class RepairController {
     @Autowired
     private RepairService repairService;
     @Autowired
@@ -59,7 +54,7 @@ public class RepairController extends BaseController {
     @GetMapping("/getequipmentByCode/{terminalCode}")
     @ApiImplicitParam(name = "terminalCode",value = "终端编号",required = true,paramType = "path")
     public List<EquipmentVo> getEquipmentByCode(@PathVariable String terminalCode){
-        if(StringUtils.isEmpty(terminalCode)){
+        if("".equals(terminalCode)){
             throw new BizException("终端编号不能为空");
         }
        return  repairService.getEquipmentByCode(terminalCode);
@@ -73,134 +68,15 @@ public class RepairController extends BaseController {
         return repairService.getVendorList();
     }
 
-    @ApiOperation(value ="（故障）工单详情查询")
-    @PostMapping("/getRepairById")
-    public RepairVo getRepairById(@RequestBody ConditionsDto conditionsDto, HttpServletRequest request){
-        TokenUserInfo tokenUserInfo = getCurrentUserInfo(request);
-
-          if("1".equals(conditionsDto.getFlog())){
-              //系统自建
-              RepairVo repairVo= repairService.getWOrkSystemByCode(conditionsDto.getRepairCode());
-              if(repairVo == null){
-                  throw new BizException("未找到对应的故障单信息");
-              }
-              repairVo.setCreateName("admin");
-              //判断是否是现场联系人
-              if(tokenUserInfo.getUserId().equals(repairVo.getContactName())){
-                  repairVo.setIsCreateUser("0");
-              }else{
-                  repairVo.setIsCreateUser("1");
-              }
-
-
-              return repairVo;
-          }else{
-              //人工创建
-              RepairVo repairVo =  repairService.getRepairById(conditionsDto.getRepairCode());
-              if(repairVo == null){
-                  throw new BizException("未找到对应的故障单信息");
-              }
-              int i=repairService.getUserRoleById(tokenUserInfo.getUserId(),"19");
-              if(i==0){
-                  //判断是否是分行管理员
-                  int b = repairService.getUserRoleById(tokenUserInfo.getUserId(),"18");
-                  if(b==0){
-                      repairVo.setUserType("0");
-                  }else{
-                      repairVo.setUserType("2");
-                  }
-
-              }else{
-                  repairVo.setUserType("1");
-              }
-
-              //判断是否为创建人
-              String isCreateUser = repairService.getUserByCode(tokenUserInfo.getUserId(),conditionsDto.getRepairCode());
-              if(isCreateUser !=null ){
-                  repairVo.setIsCreateUser("0");
-              }else{
-                  repairVo.setIsCreateUser("1");
-              }
-              return repairVo;
-          }
-    }
-
-    @ApiOperation(value ="（投诉）工单详情查询")
-    @GetMapping("/getComplaintsRepairById/{repairCode}")
+    @ApiOperation(value ="工单详情查询")
+    @GetMapping("/getRepairById/{repairCode}")
     @ApiImplicitParam(name = "repairCode",value = "工单编号",required = true,paramType = "path")
-    public RepairVo getComplaintsRepairById(@PathVariable String repairCode, HttpServletRequest request){
+    public RepairVo getRepairById(@PathVariable String repairCode){
         if("".equals(repairCode)){
             throw new BizException("工单编号不能为空");
         }
-        TokenUserInfo tokenUserInfo = getCurrentUserInfo(request);
-
-        RepairVo repairVo= repairService.getComplaintsRepairById(repairCode);
-        if(repairVo == null){
-            throw new BizException("未找到对应的投诉单信息");
-        }
-        int i=repairService.getUserRoleById(tokenUserInfo.getUserId(),"19");
-        if(i==0){
-            //判断是否是分行管理员
-            int b = repairService.getUserRoleById(tokenUserInfo.getUserId(),"18");
-            if(b==0){
-                repairVo.setUserType("0");
-            }else{
-                repairVo.setUserType("2");
-            }
-
-        }else{
-            repairVo.setUserType("1");
-        }
-
-        //判断是否为创建人
-        String isCreateUser = repairService.getUserByCode(tokenUserInfo.getUserId(),repairCode);
-        if(isCreateUser !=null ){
-            repairVo.setIsCreateUser("0");
-        }else{
-            repairVo.setIsCreateUser("1");
-        }
-        return repairVo;
+        return repairService.getRepairById(repairCode);
     }
-
-
-    @ApiOperation(value ="（巡检）工单详情查询")
-    @GetMapping("/getInspevtionRepairById/{repairCode}")
-    @ApiImplicitParam(name = "repairCode",value = "工单编号",required = true,paramType = "path")
-    public InspectionRepairVo getInspectionRepairById(@PathVariable String repairCode , HttpServletRequest request){
-        if("".equals(repairCode)){
-            throw new BizException("工单编号不能为空");
-        }
-        TokenUserInfo tokenUserInfo = getCurrentUserInfo(request);
-        InspectionRepairVo inspectionRepairVo= repairService.getInspectionRepairById(repairCode);
-        if(inspectionRepairVo == null){
-            throw new BizException("未找到对应的巡检单信息");
-        }
-        int i=repairService.getUserRoleById(tokenUserInfo.getUserId(),"19");
-        if(i==0){
-            //判断是否是分行管理员
-            int b = repairService.getUserRoleById(tokenUserInfo.getUserId(),"18");
-            if(b==0){
-                inspectionRepairVo.setUserType("0");
-            }else{
-                inspectionRepairVo.setUserType("2");
-            }
-
-        }else{
-            inspectionRepairVo.setUserType("1");
-        }
-
-        //判断是否为陪同人员
-        String isCreateUser = repairService.getAccompaniedByCode(tokenUserInfo.getUserId(),repairCode);
-        if(isCreateUser !=null ){
-            //0:是陪同人员 1:否
-            inspectionRepairVo.setIsCreateUser("0");
-        }else{
-            inspectionRepairVo.setIsCreateUser("1");
-        }
-        return inspectionRepairVo;
-    }
-
-
 
 
     @ApiOperation(value ="巡检工单新增--设备信息查询")
@@ -226,7 +102,7 @@ public class RepairController extends BaseController {
     @ApiOperation(value ="设备管理首页-设备状态")
     public DevicesNumberVo getDevicesNumber(@PathVariable String orgId){
 
-        return  repairService.getDevicesNumber(orgId);
+        return  repairService. getDevicesNumber(orgId);
     }
 
     @PostMapping("/getLargerScreen")
@@ -246,82 +122,22 @@ public class RepairController extends BaseController {
 
       @PostMapping("/getWorkOrder")
       @ApiOperation(value ="工单列表查询")
-      public Map<String, Object> getWorkOrder(@RequestBody WorkOrdersDto workOrdersDto){
-          Map<String, Object> resultMap = new HashMap<>();
+      public IPage<WorkOrderVO> getWorkOrder(@RequestBody WorkOrdersDto workOrdersDto){
         if("4".equals(workOrdersDto.getSourceType())){
-            //系统自建
             IPage<WorkOrderVO> workOrderList = repairService.getWorkOrderBySystem(workOrdersDto);
-
-            resultMap.put("records", workOrderList.getRecords());
-            resultMap.put("current", workOrderList.getCurrent());
-            resultMap.put("size", workOrderList.getSize());
-            resultMap.put("total", workOrderList.getTotal());
-
-            return resultMap;
-        }else if("5".equals(workOrdersDto.getSourceType())){
-            List<WorkOrderVO> list = repairService.getWorkOrderList(workOrdersDto);
-            List<WorkOrderVO> workOrderList=null;
-            if (workOrdersDto.getWorkOrderType().equals("01")) {
-                   //故障单加上系统自建的
-                workOrderList = repairService.getWorkOrderBySystemList(workOrdersDto);
-
-            }
-
-            if(!CollectionUtils.isNotEmpty(workOrderList)){
-                workOrderList =new ArrayList<>();
-            }
-
-            if(CollectionUtils.isNotEmpty(list)){
-                list.addAll(workOrderList);
-                //按时间排序
-                listSort(list);
-                //分页
-                resultMap.put("total", list.size());
-                resultMap.put("current", workOrdersDto.getPageIndex());
-                resultMap.put("size", workOrdersDto.getPageSize());
-
-                if(CollectionUtils.isNotEmpty(list)){
-                    int totalCount = list.size();
-                    int page =workOrdersDto.getPageSize();
-                    int pageIndex =workOrdersDto.getPageIndex();
-                    int tem =page * pageIndex;
-                    if(tem>=totalCount){
-                       tem =totalCount;
-                    }
-                    int toIndex =(pageIndex-1)*workOrdersDto.getPageSize();
-                    if(toIndex > totalCount){
-                        toIndex =totalCount;
-                    }
-
-                    resultMap.put("records", list.subList(toIndex,tem));
-                }
-                return resultMap;
-            }
-
+//            List<WorkOrderVO> list = workOrderList.getRecords();
+//            //获取机构名称
+//            for(int i=0;i<list.size();i++){
+//                String name=   repairService.getBuffetLine(list.get(i).getOrgId());
+//                if(!"".equals(name) || null != name){
+//                    list.get(i).setOrgName(name);
+//                }
+//
+//            }
+           return workOrderList;
         }
-
         return repairService.getWorkOrder(workOrdersDto);
       }
-
-    private void listSort(List<WorkOrderVO> list) {
-            Collections.sort(list, new Comparator<WorkOrderVO>() {
-                @Override
-                public int compare(WorkOrderVO o1, WorkOrderVO o2) {
-                    try {
-                       if(o1.getCreateTime().getTime()<o2.getCreateTime().getTime()){
-                           return 1;
-                       }else if(o1.getCreateTime().getTime()>o2.getCreateTime().getTime()){
-                           return -1;
-                       }else{
-                           return 0;
-                       }
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    return 0;
-                }
-            });
-    }
 
     @ApiOperation(value ="（主管，工程师收到故障单）人工创建工单信息查询")
     @GetMapping("/getBreakWorkOrderByCode/{repairCode}")
@@ -372,13 +188,13 @@ public class RepairController extends BaseController {
             workWater.setSerialNumber(Tools.getFreeOrderNo());
             workWater.setWordOrderId(workOrderCode);
             workWater.setDealWithType("1");
-            workWater.setDealWithTime(new Date());
+            workWater.setDealWithTime(LocalDateTime.now());
             workWater.setOrgId(repairRebackVo.getOrgId());
             workWater.setDealWithPeopleId(repairRebackVo.getDealWithPeopleId());
             workWater.setDealWithPeopleName(repairRebackVo.getDealWithPeopleName());
-            workWater.setDealWithPeopleRole(Integer.parseInt(repairRebackVo.getDealWithPeopleRole()));
+            workWater.setDealWithPeopleRole(Integer.valueOf(repairRebackVo.getDealWithPeopleRole()));
             workWater.setDealWithNote(repairRebackVo.getDealWithNote());
-            workWater.setCreateTime(new Date());
+            workWater.setCreateTime(LocalDateTime.now());
             workWater.setPhone(repairRebackVo.getPhone());
             workWater.setOperationType("8");
             flag =  manageWorkWaterService.save(workWater);
@@ -388,48 +204,45 @@ public class RepairController extends BaseController {
     }
 
 
-    @ApiOperation(value ="工单(操作)")
+    @ApiOperation(value ="工单评论")
     @PostMapping("/repairComment")
-    public Boolean repairComment(@RequestBody RepairCommentVo commentVo, HttpServletRequest request){
+    public Boolean repairComment(@RequestBody RepairCommentVo commentVo){
+        Boolean flag = false;
         if(null == commentVo.getWorkOrderCode()  || "".equals(commentVo.getWorkOrderCode())){
             throw new BizException("工单编号不能为空");
         }
-        TokenUserInfo tokenUserInfo = getCurrentUserInfo(request);
-        Boolean flag =  repairService.wordOperation(tokenUserInfo,commentVo);
+        String workOrderCode = commentVo.getWorkOrderCode() ;
+        RepairVo repairVo = repairService.getRepairById(workOrderCode);
+        if(repairVo != null) {
+            //更新工单状态为已评论
+            LambdaUpdateWrapper<ManageWorkOrderDO> lambdaUpdateWrapper = new LambdaUpdateWrapper<>();
+            lambdaUpdateWrapper.eq(ManageWorkOrderDO::getWorkOrderCode,workOrderCode).set(ManageWorkOrderDO::getWorkOrderStatus,"10")
+            .set(ManageWorkOrderDO::getRating,commentVo.getRating())
+            .set(ManageWorkOrderDO::getRatingNote,commentVo.getRatingNote());
+            flag = repairService.update(lambdaUpdateWrapper);
+        }
         return flag ;
     }
 
     @ApiOperation(value ="工单流转历史")
     @GetMapping("/repairHistory/{repairCode}")
     @ApiImplicitParam(name = "repairCode",value = "工单编号",required = true,paramType = "path")
-    public List<RepairHistoryListVo> repairHistory(@PathVariable String repairCode){
-        List<RepairHistoryListVo> list = new ArrayList<>();
+    public Object repairHistory(@PathVariable String repairCode){
+        Map<String,Object> map = new HashMap<>();
         if("".equals(repairCode) || null == repairCode ){
             throw new BizException("工单编号不能为空");
         }
-        list = repairService.getRepairHistoryList(repairCode);
-        //获取图片id
-        List<PictureVo> pictureVos =repairService.getPictureByCode(repairCode);
-        if(pictureVos.size()!=0){
-             List<String> li =new ArrayList<>();
-            //获取图片路径
-            ImagePlatformUtils imagePlatformUtils = new ImagePlatformUtils("zzsbgl");
-            for(PictureVo pictureVo : pictureVos){
-                ImagePlatformResponse response = imagePlatformUtils.query(pictureVo.getId(), pictureVo.getStartTime());
-                List<ImagePlatformFile> imagePlatformFiles= response.getImagePlatformFileList();
-                for(ImagePlatformFile imagePlatformFile : imagePlatformFiles){
-                    li.add(imagePlatformFile.getHttpFilePath());
-                }
-            }
-
-            //保存路径
-            for( RepairHistoryListVo repairHistoryListVos :list){
-                repairHistoryListVos.setThumbs(li);
-            }
+        RepairVo repairVo = repairService.getRepairById(repairCode);
+        if(repairVo != null) {
+            ManageWorkOrderDO workOrderDO = repairService.getById(repairVo.getId());
+            //截图URL的list
+            QueryWrapper<ManageWorkOrderAttachmentDO> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("repair_id",repairCode);
+            List<ManageWorkOrderAttachmentDO>  list = manageWorkOrderAttachmentService.list(queryWrapper);
+            map.put("repairHistory",workOrderDO);
+            map.put("imageList",list);
         }
-
-
-        return  list;
+        return  map;
     }
 
     @ApiOperation(value ="自助设备列表查询")
@@ -445,27 +258,6 @@ public class RepairController extends BaseController {
             throw new BizException("id不能为空");
         }
         return repairService.getKioskById(id);
-
-    }
-
-    @ApiOperation(value ="查询所有的自助行")
-    @PostMapping("/getBuffetLineList")
-    public List<BuffetLineVo> getBuffetLineList(@RequestBody BuffetLineDto buffetLineDto){
-        return repairService.getBuffetLineList(buffetLineDto);
-
-    }
-
-    @ApiOperation(value ="查询所有的分行")
-    @GetMapping("/getBranchVoList")
-    public List<BuffetLineVo> getBranchVoList(){
-        return repairService.getBranchVoList();
-
-    }
-
-    @ApiOperation(value ="查询所有的支行")
-    @GetMapping("/getSubbranchList/{code}")
-    public List<BuffetLineVo> getSubbranchList(@PathVariable String code){
-        return repairService.getSubbranchList(code);
 
     }
 }
